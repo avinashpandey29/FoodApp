045c45f5a1017fa5f1dc2868a90cda639c959a36
Avinash Pandey <avinash.cit.cse@gmail.com>
2021-07-25 11:45:39 +0530

    Final commit

---
+++ src/order/order-summary-view.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+import Button from "@material-ui/core/Button";
+
+export default class OrderSummaryView extends Component {
+  render() {
+    return (
+      <div className="order-summary-view">
+        <h4 className="title">{this.props.courseTitle}</h4>
+        <ul>
+          {this.props.courseItems.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+
+
+      </div>
+
+    )
+  }
+}
---
+++ src/order/order-summary.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+import { getCourseName } from "../utils/courseName"
+import OrderSummaryView from "./order-summary-view"
+import Button from "@material-ui/core/Button";
+
+export default class OrderSummary extends Component {
+  render() {
+    const { selectedItems } = this.props
+    return Object.keys(selectedItems).map(item => {
+      if (selectedItems[item].length !== 0) {
+        let courseItems = []
+        for (let i = 0; i < selectedItems[item].length; i++) {
+          courseItems.push(selectedItems[item][i])
+        }
+        return (
+
+          <OrderSummaryView
+            key={item}
+            courseTitle={getCourseName(item)}
+            courseItems={courseItems}
+          />
+
+        )
+      }
+    })
+
+  }
+}
